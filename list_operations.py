49e261657b2da830f58b2a275878e2a8952ea214
Naomi <naomi.munyiri11@gmail.com>
2024-04-14 14:16:37 +0300

    list operations

---
+++ list_operations.py
@@ -0,0 +1,10 @@
+numbers=[4,3,7,1,5]
+print(numbers)
+
+numbers.sort()#sorts in ascending order
+print(numbers)
+
+numbers.sort(reverse=True)#sorts in descending order
+print(numbers)
+
+print(len(numbers))#length of the list

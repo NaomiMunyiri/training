87edac541bdaa34d25f9e4821e9b04d808cccdf2
Naomi <naomi.munyiri11@gmail.com>
2024-04-22 15:36:53 +0300

    if statements

---
+++ if_statement.py
@@ -0,0 +1,19 @@
+# fruits=["apple","banana","watermelon","avocado"]
+
+# for fruit in fruits:
+#     if fruit=="watermelon":
+#         print(fruit.upper())
+
+#     else:
+#         print(fruit.title())
+
+
+fruits=["apple","banana","watermelon"]
+requested_fruit="banana"
+
+# for fruit in fruits:
+#     if fruit != requested_fruit or fruit == "apple":
+#         print(f"{fruit} is not a banana")
+
+
+print('banana' in fruits)
